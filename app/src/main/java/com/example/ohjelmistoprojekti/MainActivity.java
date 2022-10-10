5cc02cf34acf0d85ed8d54be6fa51e7966d251d5
Daniil Kovalev <61245613+BoltVolta@users.noreply.github.com>
2022-10-10 16:10:22 +0300

    Notifications and Graphs base
    Basic dummy notifications and some basic graphs.

--- app/src/main/java/com/example/ohjelmistoprojekti/MainActivity.java
+++ app/src/main/java/com/example/ohjelmistoprojekti/MainActivity.java
@@ -1,31 +1,51 @@
 package com.example.ohjelmistoprojekti;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
+    String CHANNEL_ID = "NotificationChannelOne";                                       //Notifikaatio kanavan Id
+    String CHANNEL_NAME = "BasicNotification";                                          //Notifikaatio kanavan nimi
+    String description = "This is a Demo Notification with no proper functionality";    //Notifikaatio kanavan kuvaus
 
-    HomeFragment homeFragment = new HomeFragment();
-    HappyHourFragment happy_hourFragment = new HappyHourFragment();
-    SettingsFragment settingsFragment = new SettingsFragment();
+    BottomNavigationView bottomNavigationView;                                          //Navigointipalkin viite
+
+    HomeFragment homeFragment = new HomeFragment();                                     //
+    HappyHourFragment happy_hourFragment = new HappyHourFragment();                     //
+    SettingsFragment settingsFragment = new SettingsFragment();                         //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        replaceFragment(homeFragment);
+        createNotificationChannel();
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
+        replaceFragment(homeFragment);                                                  // APPia avatessa se avaa koti fragmentin
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);                 // Tunnistaa navigointipalkin
+        bottomNavigationView.setOnItemSelectedListener(item -> {                        // navigatiopalkin kuuntelija
 
-            switch(item.getItemId()){
+            switch(item.getItemId()){                                                   //
                 case R.id.home:
                     replaceFragment(homeFragment);
                     break;
@@ -39,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
-    }
 
+    }
 
     private void replaceFragment(Fragment fragment){
 
@@ -49,4 +69,36 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.flFragment,fragment);
         fragmentTransaction.commit();
     }
+
+    public void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void buildNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.stat_notify_sync)
+                .setContentTitle("Electricity Prices")
+                .setContentText("Price has dipped below your input threshold")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+    }
+
+    /*public void createChart(ArrayList array){
+        BarChart barChart = findViewById(R.id.Chart1);
+        BarDataSet barDataSet = new BarDataSet(array, "Electricity Price Dummy");
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(true);
+    }*/
+
 }
