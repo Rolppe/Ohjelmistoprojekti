13b35e3ee5ba0132de09b8957f33e03ca97f14e2
Daniil Kovalev <61245613+BoltVolta@users.noreply.github.com>
2022-10-10 17:26:56 +0300

    Chart error fix

--- app/src/main/java/com/example/ohjelmistoprojekti/HomeFragment.java
+++ app/src/main/java/com/example/ohjelmistoprojekti/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        //();
+        getData();
         ((MainActivity) requireActivity()).createChart(dummyDataArrayList);
         return view;
     }
