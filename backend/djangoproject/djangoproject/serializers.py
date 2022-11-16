c133f021f10e76f0f993e011c68e53d74d279e22
Mikko Paaso <mikko@Mikko-MacBook-Pro.local>
2022-11-16 16:46:13 +0200

    url request implemented

--- backend/djangoproject/djangoproject/serializers.py
+++ backend/djangoproject/djangoproject/serializers.py
@@ -9,4 +9,4 @@ class LuokannimiSerializer(serializers.ModelSerializer):
 class PricesSerializer(serializers.ModelSerializer):
     class Meta:
         model = DayPrices
-        fields = ['id','Time', 'H00','H01','H02','H03','H04','H05','H06','H07','H08','H09','H10','H11','H12','H13','H14','H15','H16','H17','H18','H19','H20','H21','H22','H23']
+        fields = ['id','Date', 'H00','H01','H02','H03','H04','H05','H06','H07','H08','H09','H10','H11','H12','H13','H14','H15','H16','H17','H18','H19','H20','H21','H22','H23']
--- backend/djangoproject/djangoproject/views.py
+++ backend/djangoproject/djangoproject/views.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
 from .models import Luokannimi, DayPrices
 from .serializers import LuokannimiSerializer, PricesSerializer
+from .python.get_request import  combineShite, updateHandler
+from django.db import models
+import datetime
+
+
 
 def LuokannimiLista(request): # TÄMÄ ON REQUEST HANDLER
-
-
     # Get all Luokannimi items
     luokat = Luokannimi.objects.all()
 
@@ -16,33 +19,10 @@ def LuokannimiLista(request): # TÄMÄ ON REQUEST HANDLER
     # Safe false sallii muiden kun listojen muuttamisen jsoniksi
 
 def PriceRequestHandler(request):
-    price_instance = DayPrices.objects.create(
-        H00 = 10, 
-        H01 = 20,
-        H02 = 30,
-        H03 = 40,
-        H04 = 50,
-        H05 = 60,
-        H06 = 70,
-        H07 = 80,
-        H08 = 90,
-        H09 = 80,
-        H10 = 70,
-        H11 = 60,
-        H12 = 50,
-        H13 = 40,
-        H14 = 30,
-        H15 = 20,
-        H16 = 10,
-        H17 = 20,
-        H18 = 30,
-        H19 = 40,
-        H20 = 50,
-        H21 = 60,
-        H22 = 70,
-        H23 = 80
-    )
-    # Get all Luokannimi items
+    
+    # Update database
+    updateHandler()
+    # Get all DayPrices items
     all_prices = DayPrices.objects.all()
 
     # Serialize them
