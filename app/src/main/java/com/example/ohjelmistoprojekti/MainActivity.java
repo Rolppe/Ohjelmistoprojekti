afbdbf0323ca4219db5eec63a38186886ed6f280
Waltteri Lehtinen <waltteri.lehtinen@tuni.fi>
2022-12-03 11:09:32 +0200

    testing different things, don't mind about this commit

--- app/src/main/java/com/example/ohjelmistoprojekti/MainActivity.java
+++ app/src/main/java/com/example/ohjelmistoprojekti/MainActivity.java
@@ -1,26 +1,33 @@
 package com.example.ohjelmistoprojekti;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
-
+import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     String CHANNEL_ID = "NotificationChannelOne";                                       //Notifikaatio kanavan Id
     String CHANNEL_NAME = "BasicNotification";                                          //Notifikaatio kanavan nimi
     String description = "This is a Demo Notification with no proper functionality";    //Notifikaatio kanavan kuvaus
+
+    private RequestQueue queue;
+    private Double[] priceArr = new Double[24];
 
     BottomNavigationView bottomNavigationView;                                          //Navigointipalkin viite
 
@@ -33,7 +40,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toteutetaan RequestQueue
+        queue = Volley.newRequestQueue(this);
+
         createNotificationChannel();
+        getPriceData();
 
         replaceFragment(homeFragment);                                                  // APPia avatessa se avaa koti fragmentin
         bottomNavigationView = findViewById(R.id.bottomNavigationView);                 // Tunnistaa navigointipalkin
@@ -71,6 +82,50 @@ public class MainActivity extends AppCompatActivity {
             channel.setDescription(description);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+    public void getPriceData() {
+
+        Toast.makeText(this, "getPriceData -funktion sisällä!", Toast.LENGTH_SHORT).show();
+        // Haetaan hintatiedot palvelimelta (JSON)
+        String url = "https://ohjelmistoprojekti-production.up.railway.app/pricejson/";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    // Näytetään response toastina ruudulla
+                    Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+
+                    parseJsonAndUpdateUI(response);
+                },
+                error -> {
+                    // virhe verkosta hakemisessa
+                    Toast.makeText(this, "Verkkovirhe", Toast.LENGTH_SHORT).show();
+                }
+        );
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+    private void parseJsonAndUpdateUI(String response) {
+        try {
+            JSONObject rootObject = new JSONObject(response);
+
+            // Parsitaan JSONia eli kaivetaan haluttu data
+            String id = rootObject.getJSONArray("Prices").getJSONObject(8).getString("id");
+            Double H23 = rootObject.getJSONArray("Prices").getJSONObject(8).getDouble("H23");
+            priceArr[0] = H23;
+            String sH23 = String.valueOf(priceArr[0]);
+
+            Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, sH23, Toast.LENGTH_SHORT).show();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Virhe JSON parsinnassa", Toast.LENGTH_SHORT).show();
         }
     }
 }
